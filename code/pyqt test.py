2692608bb7d41d021bbfa629157a45b8b2a33766
Wenjie ZHANG <z1w1j@163.com>
2018-02-10 19:14:47 +0800

    Add files via upload

---
+++ code/pyqt test.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt4 import QtGui
+'''
+app = QtGui.QApplication(sys.argv)
+
+window = QtGui.QWidget()
+window.setGeometry(50,50,500,400)
+window.setWindowTitle("EBL")
+window.show()
+
+sys.exit(app.exec_())
+'''
+def t():
+	return 1,2
+
+a,b=t()
+print a,b
